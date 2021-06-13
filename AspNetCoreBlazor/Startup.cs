693c12b72c4f08b75066021a8c13867f43ae1928
Moacyr Rodrigues Pereira <moacyr@m3sistemas.com.br>
2021-06-13 10:20:23 -0300

    Adicionado setup EF

--- AspNetCoreBlazor/Startup.cs
+++ AspNetCoreBlazor/Startup.cs
@@ -1,8 +1,10 @@
 using AspNetCoreBlazor.Data;
+using Domain.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -26,6 +28,8 @@ namespace AspNetCoreBlazor
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<EpiContext>(options => options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+
             services.AddRazorPages();
             services.AddServerSideBlazor();
             services.AddSingleton<WeatherForecastService>();
